c85297c36a43c6c98121de6f8070c948be8153be
Matthew Robinson <matt_robinson1@yahoo.com>
2019-08-27 17:19:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,15 +1,4 @@
 def nyc_pigeon_organizer(data)
-   hash = {}
 
-  data.each do |att, attr_data|
-    attr_data.each do |attr_ex, names_arr|
-      names_arr.each do |name|
-        hash[name] = {} if !hash[name]
-        hash[name][att] = [] if !hash[name][att]
-        hash[name][att] << attr_ex.to_s
-      end
-    end
-  end
-p_hash
-end
+end 
 
